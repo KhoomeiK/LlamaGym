42c07c357c41364321f3860e62ad7948806a24b1
khoomeik <32777448+KhoomeiK@users.noreply.github.com>
2024-02-29 21:11:53 -0800

    agent and blackjack example

---
+++ blackjack.py
@@ -0,0 +1,80 @@
+from tqdm import trange
+
+from transformers import AutoTokenizer
+from peft import LoraConfig
+from trl import AutoModelForCausalLMWithValueHead
+
+import gymnasium as gym
+from llamagym.agent import Agent
+
+class BlackjackAgent(Agent):
+    def get_system_prompt(self) -> str:
+        return """You are an expert single-player blackjack player. Every turn, you'll see your current sum, the dealer's showing card value, and whether you have a usable ace. You win by (1) not exceeding 21, and (2) exceeding the dealer's hand -- hidden+showing card values.
+Decide whether to stick or hit by writing "Action: 0" or "Action: 1" respectively. You MUST decide your action and remember to write "Action:"."""
+
+    def format_observation(self, observation: gym.core.ObsType) -> str:
+        return f"Sum is {observation[0]}, dealer is {observation[1]}, {'have' if bool(observation[2]) else 'no'} ace"  # # i.e. Sum is 14, dealer is 6, no ace.
+
+    def extract_action(self, response: str) -> gym.core.ActType:
+        digits = [char for char in response if char.isdigit()]
+        if len(digits) == 0 or digits[-1] not in ("0", "1"):
+            if "stick" in response.lower():
+                return 0
+            elif "hit" in response.lower():
+                return 1
+            raise ValueError("No action chosen")
+        else:
+            return int(digits[-1])
+
+if __name__ == "__main__":
+    hyperparams = {
+        "model_name": "meta-llama/Llama-2-7b-chat-hf",
+        "env": "Blackjack-v1",
+        "lora/r": 16,
+        "lora/lora_alpha": 32,
+        "lora/lora_dropout": 0.05,
+        "lora/bias": "none",
+        "lora/task_type": "CAUSAL_LM",
+        "load_in_8bit": True,
+        "batch_size": 16,
+        "seed": 42069,
+        "episodes": 5000,
+        "generate/max_new_tokens": 32,
+        "generate/do_sample": True,
+        "generate/top_p": 0.6,
+        "generate/top_k": 0,
+        "generate/temperature": 0.9
+    }
+    device = "cuda:0"
+    HF_TOKEN = None
+
+    lora_config = LoraConfig(
+        **{key.split('/')[-1]: value for key, value in hyperparams.items() if key.startswith('lora/')}
+    )
+
+    model = AutoModelForCausalLMWithValueHead.from_pretrained(
+        pretrained_model_name_or_path=hyperparams["model_name"],
+        peft_config=lora_config,
+        load_in_8bit=hyperparams["load_in_8bit"],
+        token=HF_TOKEN
+    ).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(hyperparams["model_name"], token=HF_TOKEN)
+    tokenizer.add_special_tokens({"pad_token": "<pad>"})
+    model.pretrained_model.resize_token_embeddings(len(tokenizer))
+    
+    agent = BlackjackAgent(model, tokenizer, 
+                           {key: value for key, value in hyperparams if key.startswith('generate/')}, 
+                           {'batch_size': hyperparams['batch_size']})
+    env = gym.make(hyperparams['env'], natural=False, sab=False)
+
+    for episode in trange(hyperparams['episodes']):
+        observation, info = env.reset(seed=hyperparams['seed'])
+        done = False
+
+        while not done:
+            action = agent.act(observation)
+            observation, reward, terminated, truncated, info = env.step(action)
+            agent.assign_reward(reward)
+            done = terminated or truncated
+        
+        train_stats = agent.terminate_episode()
---
+++ llamagym/agent.py
@@ -0,0 +1,131 @@
+from abc import ABC, abstractmethod
+
+import gymnasium as gym
+import torch
+from trl import (
+    PPOTrainer,
+    PPOConfig,
+    create_reference_model,
+)
+
+class Agent(ABC):
+    def __init__(self, model, tokenizer, generate_config_dict, ppo_config_dict):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.generate_config_dict = generate_config_dict
+        self.device = self.model.device
+        self.model_ref = create_reference_model(model)
+        self.ppo_config = PPOConfig(batch_size=ppo_config_dict['batch_size'])
+        self.ppo_trainer = PPOTrainer(self.ppo_config, model, self.model_ref, tokenizer)
+
+        self.current_batch = {"queries": [], "responses": [], "rewards": []}
+
+        self.current_episode_messages = [
+            {
+                "role": "system",
+                "content": self.get_system_prompt(),
+            }
+        ]
+        self.current_episode_rewards = []
+    
+    @abstractmethod
+    def get_system_prompt(self) -> str:
+        pass
+
+    @abstractmethod
+    def format_observation(self, observation: gym.core.ObsType) -> str:
+        pass
+
+    @abstractmethod
+    def extract_action(self, response: str) -> gym.core.ActType:
+        pass
+
+    def llm(self, messages) -> str:
+        prompt = self.tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
+        generate_ids = self.model.generate(
+            inputs=inputs.input_ids,
+            **{key.split('/')[-1]: value for key, value in self.generate_config_dict.items()}
+        )
+        outputs = self.tokenizer.batch_decode(
+            generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+
+        response = outputs[0].split("<|assistant|>\n")[-1]
+        return response
+
+    def act(self, observation):
+        message = self.format_observation(observation)
+        self.current_episode_messages += [{"role": "user", "content": message}]
+
+        response = self.llm(self.current_episode_messages)
+        try:
+            action = self.extract_action(response)
+        except Exception as e:
+            return None
+
+        self.current_episode_messages += [{"role": "assistant", "content": response}]
+        return action
+    
+    def assign_reward(self, reward):
+        self.current_episode_rewards.append(reward)
+    
+    def format_episode_for_ppo(self, messages, rewards):
+        queries, responses = [], []
+        for i in range(1, len(messages), 2):
+            prompt = self.tokenizer.apply_chat_template(
+                messages[: i + 1], tokenize=False, add_generation_prompt=False
+            )
+            conversation_chunks = prompt.split("<|assistant|>\n")
+            query = "<|assistant|>\n".join(conversation_chunks[:-1]) + "<|assistant|>\n" # TODO: ensure query is user and response is assistant
+            response = conversation_chunks[-1][:-5]  # remove final "</s>\n"
+
+            query = self.tokenizer(query, return_tensors="pt").input_ids[0]
+            response = self.tokenizer(response, return_tensors="pt").input_ids[0]
+
+            queries.append(query)
+            responses.append(response)
+
+        if all(reward == 0 for reward in rewards[:-1]):
+            # if sparse rewards, give equal reward to all conversation turns
+            per_turn_reward = rewards[-1] / (
+                len(messages) / 2
+            )
+            rewards = [torch.tensor(per_turn_reward, dtype=torch.float16)] * len(queries)
+        else:
+            rewards = [torch.tensor(reward, dtype=torch.float16) for reward in rewards]
+
+        return queries, responses, rewards
+
+    def terminate_episode(self):
+        queries, responses, rewards = self.format_episode_for_ppo(self.current_episode_messages, self.current_episode_rewards)
+        self.current_batch["queries"].extend(queries)
+        self.current_batch["responses"].extend(responses)
+        self.current_batch["rewards"].extend(rewards)
+
+        if len(self.current_batch["queries"]) >= self.ppo_config.batch_size:
+            train_stats = self.train_batch(self.current_batch["queries"], self.current_batch["responses"], self.current_batch["rewards"])
+            return train_stats
+            
+    def train_batch(self, batch_queries, batch_responses, batch_rewards):
+        if len(queries) > self.ppo_config.batch_size:
+            queries = batch_queries[: self.ppo_config.batch_size]
+            responses = batch_responses[: self.ppo_config.batch_size]
+            rewards = batch_rewards[: self.ppo_config.batch_size]
+
+            # keep the remainder for the next batch
+            self.current_batch["queries"] = batch_queries[self.ppo_config.batch_size :]
+            self.current_batch["responses"] = batch_responses[self.ppo_config.batch_size :]
+            self.current_batch["rewards"] = batch_rewards[self.ppo_config.batch_size :]
+        else:
+            queries, responses, rewards = batch_queries, batch_responses, batch_rewards
+            self.current_batch = {"queries": [], "responses": [], "rewards": []}
+
+        train_stats = self.ppo_trainer.step(
+            queries, responses, rewards
+        )
+        torch.cuda.empty_cache()
+
+        return train_stats
